cdf9bf303b2acc82d611e3e4b05f653fa6b82181
Gustavo Esteves <gustavoalves19992@gmail.com>
2021-07-07 20:28:49 -0300

    Mudanças nos arquivos JS para TS

---
+++ TypeScript_Parte_1/alurabank/app/ts/models/negociacao.ts
@@ -0,0 +1,35 @@
+/**
+ *@author Gustavo Esteves
+ * Criada para fazer as principais negociações
+ * @class Negociacao
+ */
+class Negociacao {
+  private _data;
+  private _quantidade;
+  private _valor;
+
+  constructor(data, quantidade, valor) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
+
+  get data() {
+    return this._data
+  }
+
+  get valor() {
+    return this._valor
+  }
+
+  get quantidade() {
+    return this._quantidade
+  }
+
+  get volume() {
+    return this._quantidade * this._valor
+  }
+
+
+
+}
